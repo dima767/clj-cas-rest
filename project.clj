304430f8bad129d94d202ac4c3ea5e3ca9ff5438
Dmitriy Kopylenko <dima767@gmail.com>
2011-11-01 17:03:08 -0400

    Clean up dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject clj-cas-rest "1.0.0-SNAPSHOT"
   :description "CAS REST Client written in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-								[clj-github "1.1.0-alpha1"]
-								[org.clojure/data.json "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]								
 								[clj-http "0.2.1"]])
